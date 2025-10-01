2dcd2bb4468d827d9b2e8a371e63053db298ddad
0xwb7 <woobin200111@gmail.com>
2025-10-01 11:41:30 +0900

    delete @data annotation in reponse, request

--- 이우빈/restapi/src/main/java/gdg/restapi/dto/request/BookRequest.java
+++ 이우빈/restapi/src/main/java/gdg/restapi/dto/request/BookRequest.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class BookRequest {
 
     private String title;
     private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
--- 이우빈/restapi/src/main/java/gdg/restapi/dto/response/BookResponse.java
+++ 이우빈/restapi/src/main/java/gdg/restapi/dto/response/BookResponse.java
@@ -4,12 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class BookResponse {
 
-    private Long id;
-    private String title;
-    private String author;
+    private final Long id;
+    private final String title;
+    private final String author;
+
+    public BookResponse(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
