f6123436efa833a48c064698797de00e70e9ff98
Jack <devjack050700@gmail.com>
2025-10-03 15:55:03 +0900

    feat: 입력 검증 추가
    - validation 의존성 추가
    - LectureRequest에 검증 어노테이션 추가
    - GlobalExceptionHandler에 유효성 검증 예외 처리 추가

--- 임정혁/ILoveYH/src/main/java/gdg/iloveyh/lecture/dto/LectureRequest.java
+++ 임정혁/ILoveYH/src/main/java/gdg/iloveyh/lecture/dto/LectureRequest.java
@@ -1,5 +1,9 @@
 package gdg.iloveyh.lecture.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +12,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LectureRequest {
+    
+    @NotBlank(message = "강의 제목은 필수입니다")
+    @Size(max = 100, message = "강의 제목은 100자를 초과할 수 없습니다")
     private String title;
+    
+    @NotBlank(message = "강의 설명은 필수입니다")
+    @Size(max = 1000, message = "강의 설명은 1000자를 초과할 수 없습니다")
     private String description;
+    
+    @NotNull(message = "강의 가격은 필수입니다")
+    @Min(value = 0, message = "강의 가격은 0원 이상이어야 합니다")
     private Long price;
 }
